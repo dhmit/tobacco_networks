65e36b2b6a8e1f5d278961aedcaaf813450a96c1
Ryaan Ahmed <rahmed@mit.edu>
2019-09-26 13:34:50 -0400

    add basic 3 panel view for visualization

--- frontend/src/main/main.js
+++ frontend/src/main/main.js
@@ -1,27 +1,99 @@
 import React from 'react';
-// import PropTypes from 'prop-types';  // uncomment once something has props!
+import PropTypes from 'prop-types';
 
 import { getCookie } from '../common'
 import './main.css';
 
 
+
 /**
- * Main component for the student view.
- *
- * Handles all logic, displays information, and makes database query/posts
+ * Controls and settings components for the visualization
+ */
+class Controls extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            show_names: false,
+        };
+    }
+
+    toggle_checkbox() {
+        this.setState({
+            show_names: !this.state.show_names,
+        });
+    }
+
+    render() {
+        return (
+            <div className="col-3">
+                <input
+                    type="checkbox"
+                    checked={this.state.show_names}
+                    onChange={() => this.toggle_checkbox()}
+                />
+                <label>Show names</label>
+            </div>
+        );
+    }
+}
+
+/**
+ * The Visualization Itself!
+ */
+class Viz extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <div className="col-6">Hello, Sailor!</div>
+        );
+    }
+}
+
+/**
+ * Info panel - data from the visualization
+ */
+class Info extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <div className="col-3">You clicked on {this.props.name}!</div>
+        );
+    }
+}
+Info.propTypes ={
+    name: PropTypes.string,
+}
+
+/**
+ * Main component for the main view.
  */
 class MainView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            hello_world: 'Hello, world!',
         };
         this.csrftoken = getCookie('csrftoken');
     }
 
     render() {
         return (
-            <div>{this.state.hello_world}</div>
+            <div className="container">
+                <div className="row">
+                    <Controls />
+                    <Viz />
+                    <Info name='NOTHING YET'/>
+                </div>
+            </div>
         );
     }
 }
