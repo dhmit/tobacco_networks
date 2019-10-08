6f35cd579bd2a0fe05d12a3c6fbffb66215377cb
Stephan Risi <stephan.risi@gmail.com>
2019-10-08 14:07:52 -0400

    Added convenience functions to access Dunn document authors.

---
+++ data_preprocessing/name_preprocessing.py
@@ -0,0 +1,67 @@
+from collections import Counter
+from pathlib import Path
+
+import pandas as pd
+
+
+def load_documents_to_dataframe() -> pd.DataFrame:
+    """
+    Loads the Dunn documents into a pandas Dataframe
+    :return: pd.DataFrame
+    """
+    df = pd.read_csv(Path('..', 'data', 'name_disambiguation', 'dunn_docs.csv')).fillna('')
+    return df
+
+
+def get_author_counter() -> Counter:
+    """
+    Returns a counter of all authors in the Dunn documents
+
+    >>> author_counter = get_author_counter()
+    >>> author_counter.most_common(1)
+    [('DUNN,WL', 2566)]
+
+    :return: Counter
+    """
+
+    df = load_documents_to_dataframe()
+    name_counter = Counter()
+    for _, row in df.iterrows():
+        for category in ['au', 'au_person']:  # don't use author organization to get authors
+            name = row[category]
+            if name:
+                for name_split_semicolon in [n.strip() for n in name.split(';')]:
+                    for name_split_bar in [m.strip() for m in name_split_semicolon.split('|')]:
+                        if len(name_split_bar) > 0:
+                            name_counter[name_split_bar] += 1
+                            if len(name_split_bar) > 100:
+                                print('unusually long name', name_split_bar)
+
+    return name_counter
+
+
+def get_authors_by_document() -> list:
+    """
+    Creates a list of documents such that each element consists of a dict with keys
+    'au', 'au_org', 'au_person'
+
+    >>> authors_by_docs = get_authors_by_document()
+    >>> authors_by_docs[0]
+    {'au': '', 'au_org': '', 'au_person': 'BOWLING,JC'}
+
+    :return: list
+    """
+
+    df = load_documents_to_dataframe()
+    authors_by_docs = []
+    for _, row in df.iterrows():
+        authors_by_docs.append({
+            'au': row['au'],
+            'au_org': row['au_org'],
+            'au_person': row['au_person']
+        })
+    return authors_by_docs
+
+
+if __name__ == '__main__':
+    get_author_counter()
