60688de75e97dce3674ec11d2f76ab3ffabdb2a1
Ryaan Ahmed <rahmed@mit.edu>
2019-10-25 11:30:17 -0400

    add jquery and popper as deps for bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';  // JS import -- just for collapse
 
 import MainView from './main/main';
 
