aa50d4bf778c5a6455944fdb55b98442a43d2089
Christina Wang <mingyew@mit.edu>
2019-11-07 10:21:03 -0500

    updated models with import and manytomany fields

--- backend/apps/main/models.py
+++ backend/apps/main/models.py
@@ -7,24 +7,85 @@ from django.db import models
 import pandas as pd
 import pickle
 from collections import Counter
+# from django.db import models
 
 from config.settings.base import BACKEND_DIR
 
-
-class Person:
-    """
-    Python class for representing people in the data;
-    probably to be eventually replaced with a Django model
-    """
-    def __init__(self, pk: int, name: str, docs: int, words: int):
-        self.pk = pk  # pylint: disable=C0103
-        self.name = name
-        self.docs = docs
-        self.words = words
-
 MAX_LENGTH = 250
 
+
+class Person(models.Model):
+    """Django database to represent Person objects
+    Fields:
+        last: CharField, last name
+        first: CharField, first name
+        middle: CharField, middle name
+        most_likely_org: CharField, org that appeared most times for the person
+        positions: TextField, string json representation of positions Counter (all related
+        annotations on the person). DO NOT DIRECTLY ACCESS (use property positions_counter)
+        aliases: TextField, string json representation of aliases Counter
+        (all raw strings used to refer to this person). DO NOT DIRECTLY ACCESS (use property
+        aliases_counter)
+        # TODO figure out if this is as author or as recipient
+        count: IntegerField, number of documents the person appeared in
+    Properties:
+        positions_counter: Counter of related annotations on the person
+        aliases_counter: Counter of all raw strings used to refer to this person
+    """
+    # Maybe should be blank = True??
+    last = models.CharField(max_length=255)
+    first = models.CharField(max_length=255)
+    middle = models.CharField(max_length=255)
+    most_likely_org = models.CharField(max_length=MAX_LENGTH)
+    # positions & aliases are json strings that need to be parsed as Counter every time
+    positions = models.TextField()
+    aliases = models.TextField()
+    count = models.IntegerField()
+
+    def __str__(self):
+        s = f'{self.first} {self.middle} {self.last}'
+        s = s + ", Position: " + str(self.positions) + ", Aliases: " + \
+            str(self.aliases) + ", count: " + str(self.count)
+        return s
+
+    @property
+    def positions_counter(self):
+        """
+        :return: positions Counter (from string json)
+        """
+        return Counter(json.loads(self.positions))
+
+    @property
+    def aliases_counter(self):
+        """
+        :return: aliases Counter (from string json)
+        """
+        return Counter(json.loads(self.aliases))
+
+
 class Document(models.Model):
+    """Django database to represent Person objects
+    # TODO: check if the field descriptions are correct
+    Fields:
+        au: CharField, author(s)
+        au_org: CharField, author organizations
+        au_person: CharField, author(s)
+        cc: CharField, cc-ed names
+        cc_org: CharField, organization of cc-ed names
+        collection: CharField, collection that the document was released from
+        date: CharField, string representation of the date of the document
+        doc_type: CharField, type of the document (e.g. letter, memo)
+        pages: IntegerField, number of pages in document
+        rc: CharField, recipient(s)
+        rc_org: CharField, recipient organizations
+        rc_person: CharField, recipient(s)
+        text: TextField, text of the document (is it full text??)
+        tid: CharField, document ID
+        title: CharField, title of the document
+
+        authors: ManyToManyField, connect Document and its authors' Person objects in Django
+        recipients: ManyToManyField, connect Document and its recipients' Person objects in Django
+    """
     au = models.CharField(blank=True, max_length=MAX_LENGTH)
     au_org = models.CharField(blank=True, max_length=MAX_LENGTH)
     au_person = models.CharField(blank=True, max_length=MAX_LENGTH)
@@ -41,14 +102,20 @@ class Document(models.Model):
     tid = models.CharField(unique=True, max_length=MAX_LENGTH)
     title = models.CharField(blank=True, max_length=MAX_LENGTH)
 
-    recipients = models.ManyToManyField(Person)
     authors = models.ManyToManyField(Person)
+    recipients = models.ManyToManyField(Person)
 
     def __str__(self):
         return f'tid: {self.tid}, title: {self.title}, date: {self.date}'
 
-def import_csv_to_document_model(path):
-    df = pd.read_csv(path).fillna('')
+
+def import_csv_to_document_model(csv_path):
+    """
+    Reads csv of docs and create Document model
+    :param csv_path: Path to csv file
+    :return:
+    """
+    df = pd.read_csv(csv_path).fillna('')
     for _, row in df.iterrows():
         d = Document(au=row['au'],
                      au_org=row['au_org'],
@@ -68,46 +135,53 @@ def import_csv_to_document_model(path):
                      )
         d.save()
 
-class Person(models.Model):
-    # Maybe should be blank = True??
-    last = models.CharField(max_length=255)
-    first = models.CharField(max_length=255)
-    middle = models.CharField(max_length=255)
-    position = models.CharField(max_length=MAX_LENGTH)
-    # TODO: Make counter into textfield as json
-    positions = models.TextField()
-    aliases = models.TextField()
-    count = models.IntegerField()
+        # for au/au_person, and rc/rc_person, parse it into list of individual raw names
+        parsed_au = []
+        if row['au_person']:
+            parsed_au = parse_column_person(row['au_person'])
+        elif row['au']:
+            parsed_au = parse_column_person(row['au'])
+        # for each raw name, search in Person model by aliases
+        # add connection to authors (ManyToManyField)
+        for name in parsed_au:
+            person = Person.objects.filter(aliases__contains=name)
+            d.authors.add(person)
 
-    # TODO: write getter to parse json in textfield
-    # See rereading/.../backend, analysis
-    # Look into JSON Fields -- they won't work here, but it's
-    # an example of how to do this stuff
-    def get_parsed_positions(self):
-        self.positions
+        parsed_rc = []
+        if row['rc_person']:
+            parsed_rc = parse_column_person(row['rc_person'])
+        elif row['rc']:
+            parsed_rc = parse_column_person(row['rc'])
+        for name in parsed_rc:
+            person = Person.objects.filter(aliases__contains=name)
+            d.recipients.add(person)
 
-    def __str__(self):
-        s = f'{self.first} {self.middle} {self.last}'
-        s = s + ", Position: " + str(self.positions) + ", Aliases: " + \
-            str(self.aliases) + ", count: " + str(self.count)
-        return s
 
-    @property
-    def positions_counter(self):
-        return Counter(json.loads(self.positions))
+def parse_column_person(column_name):
+    """
+    Splits individual names by semicolon or bar (|)
 
-    # TODO: write json getter here
-    @property
-    def aliases_list(self):
+    :param column_name: list, taken from csv with doc info
+    :return: list, names of people in column
+    """
+    names = []
+    for name_split_semicolon in [n.strip() for n in column_name.split(';')]:
+        for name_split_bar in [m.strip() for m in name_split_semicolon.split('|')]:
+            if 0 < len(name_split_bar) < 100:   # pylint: disable=C1801
+                names.append(name_split_bar)
+    return names
+
 
 def import_peopledb_to_person_model(file_path):
     with open(str(file_path), 'rb') as infile:
         db = pickle.load(infile)
+
     for person in db.people:
         p = Person(last=person.last,
                    first=person.first,
                    middle=person.middle,
-                   position=person.position,
+                   most_likely_org=person.most_likely_org,
+                   # convert Counter object into json string
                    positions=json.dumps(person.positions),
                    aliases=json.dumps(person.aliases),
                    count=person.count
@@ -115,56 +189,8 @@ def import_peopledb_to_person_model(file_path):
         p.save()
 
 
-
-class Edge:
-    """
-    Python object to store edges, should be replaced if the Person class is replaced
-    """
-    def __init__(self, pk: int, node1: str, node2: str,  # pylint: disable-msg=R0913
-                 docs: int, words: int):
-        self.pk = pk  # pylint: disable=C0103
-        self.node1 = node1  # could be replaced with Person
-        self.node2 = node2
-        self.docs = docs
-        self.words = words
-
-
-def load_network_json_data(return_type: str):
-    """
-    Loads test json data for initial prototyping
-    :param return_type: string, determines which list needs to be determined, has the
-    precondition that it either must be 'nodes' or 'edges'
-    :return: list, contains all of the edges or all of the nodes (people at this point in time)
-    from network_test_data.json
-    """
-    if return_type not in ['nodes', 'edges']:
-        raise ValueError("Specified return type needs to be nodes or edges")
-
-    json_path = Path(BACKEND_DIR, 'data', 'network_test_data.json')
-    with open(json_path) as json_file:
-        data = json.load(json_file)
-    if return_type == "edges":
-        edges = []
-        for edges_dict in data['links']:
-            pk = int(edges_dict.get('id'))  # pylint: disable=C0103
-            node1 = edges_dict.get('node1')
-            node2 = edges_dict.get('node2')
-            docs = edges_dict.get('docs')
-            words = int(edges_dict.get('words'))
-            edges.append(Edge(pk, node1, node2, docs, words))
-        return edges
-    else:
-        people = []
-        for person_dict in data['nodes']:
-            pk = int(person_dict.get('id'))  # pylint: disable=C0103
-            name = person_dict.get('name')
-            docs = person_dict.get('docs')
-            words = int(person_dict.get('words'))
-            people.append(Person(pk, name, docs, words))
-        return people
-
-
 if __name__ == '__main__':
-    path = Path('..', 'data', 'name_disambiguation', 'dunn_docs.csv')
-    import_csv_to_document_model(path)
+    import_peopledb_to_person_model(Path('..', 'data', 'name_disambiguation', 'names_db_10.pickle'))
+    import_csv_to_document_model(Path('..', 'data', 'name_disambiguation', 'dunn_docs.csv'))
     Document.objects.all()
+
