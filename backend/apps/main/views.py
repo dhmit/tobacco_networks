753026207dfd0eed793028991d31d92f1e0b66e6
Raquel Garcia <quel@mit.edu>
2019-12-05 11:42:16 -0500

    Trying to merge views
    Temporary changes in order to merge

--- backend/apps/main/views.py
+++ backend/apps/main/views.py
@@ -7,28 +7,28 @@ from pathlib import Path
 import random
 
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
-from collections import Counter
-from rest_framework.response import Response
-from .serializers import PersonInfoSerializer, EdgeSerializer, load_network_json_data
-from .models import Person
-
-
-@api_view(['GET'])
-def list_edges(request):
-    """
-    Return a list of all Edge objects, serialized.
-    """
-    serializer = EdgeSerializer(instance=load_network_json_data("edges"), many=True)
-    return Response(serializer.data)
+from backend.config.settings.base import BACKEND_DIR
 
 
 def get_network_data(request):
     """
     Temporary view to get network test data json
     """
-    json_path = Path('/Users/kimba/Documents/GitHub/tobacco_networks/backend', 'data',
-                     'person_research_directors.json')
+
+    datasets = {
+        'lawyers': 'person_lawyers.json',
+        'research_directors': 'person_research_directors.json',
+        'sterling': 'person_sterling.json',
+        'top_100_edges': 'top_100_edges.json',
+        'test': 'network_test_data.json'
+    }
+
+    if request.GET and 'dataset' in request.GET:
+        json_filename = datasets[request.GET['dataset']]
+    else:
+        json_filename = 'network_test_data.json'
+
+    json_path = Path(BACKEND_DIR, 'data', json_filename)
     with open(json_path) as json_file:
         data = json.load(json_file)
     nodes = data['nodes']
@@ -36,36 +36,18 @@ def get_network_data(request):
         node['x'] = random.random()*500
         node['y'] = random.random()*500
         node['weight'] = node['docs']
+        node['affiliation'] = random.choice(['Phillip Morris International', 'British American '
+                                                                             'Tobacco',
+                                             'Japan Tobacco', 'Imperial Tobacco'])
     links = data['links']
+    adjacent_nodes = {}
     for link in links:
         link['source'] = link['node1']
         link['target'] = link['node2']
+        adjacent_nodes[link['node1'] + "-" + link['node2']] = True
+        adjacent_nodes[link['node2'] + "-" + link['node1']] = True
+    data["adjacent_nodes"] = adjacent_nodes
+
+    # TODO: Need to add adjacent_nodes and add False value : talk to rest of group about this
+
     return JsonResponse(data)
-
-
-@api_view(['GET'])
-def get_person_info(request):
-    """
-    Finds a person matching the full name requested by the user, serializes this person, and
-    returns the serialized person
-    :param request: request from the user;
-        request.query_params is a dict {'full name': FULL NAME OF RELEVANT PERSON}
-    :return: serialized person matching full name imbain request
-    """
-    queryset = Person.objects.filter(full_name=request.query_params['full_name'])
-    if not list(queryset):
-        full_name = request.query_params['full_name']
-        new = Person(
-            last="",
-            first="",
-            middle="",
-            full_name=full_name + " not available.",
-            most_likely_org="",
-            positions=json.dumps(Counter()),
-            aliases=json.dumps(Counter()),
-            count=0)
-        serializer = PersonInfoSerializer(instance=new, many=False)
-        return Response(serializer.data)
-    else:
-        serializer = PersonInfoSerializer(instance=queryset, many=True)
-        return Response(serializer.data)
