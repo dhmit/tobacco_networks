d6fa1143b142716d393611a9f87345e31454711d
Ubuntu <ubuntu@ip-172-31-7-73.us-west-1.compute.internal>
2020-01-17 21:16:44 +0000

    update gitignore for build assets + production settings

---
+++ backend/config/settings/production.py
@@ -0,0 +1,26 @@
+"""
+
+Local development Django settings for dhmit/tobacco_networks
+
+Under no circumstances run the server with these settings in production!
+
+"""
+
+from .base import *  # pylint: disable=unused-wildcard-import, wildcard-import
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# TODO(ra): read from an environment variable!
+# TODO(ra): read from an environment variable!
+# TODO(ra): read from an environment variable!
+# TODO(ra): read from an environment variable!
+SECRET_KEY = 'qqucn931x78rx054n(6g(s_3vxppjw$f24e(9&v6rsbd0&0$2e'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    'tobacconetworks.dhmit.xyz',
+]
+
+CORS_ORIGIN_ALLOW_ALL = True
